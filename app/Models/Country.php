7119223e6749609d9538b3b9c2879c8770a51081
David wanjohi <davidwagura83@gmail.com>
2024-03-27 09:52:00 +0300

    create model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     use HasFactory;
+
+    public function posts()
+    {
+        return $this->hasManyThrough(
+            Post::class,
+            User::class,
+        );
+    }
 }
